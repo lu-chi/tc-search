b717002f0a0c6d3287970f7078ec4c420d4d5795
Lu-Chi <fra.proxy@gmail.com>
2017-11-24 10:07:20 +0100

    Rebuild tc-search; replaced with a test-search; corrected argument parsing

--- tc-search.py
+++ tc-search.py
@@ -15,8 +15,11 @@ import requests
 import json
 import argparse
 import time
+import sys
 
 url = "http://www.threatcrowd.org/searchApi/v2/{}/report/"
+#url = "http://www.threatcrowd.org/searchApi/v2/{}/malware/"
+
 wait = 10
 
 def main(args):
@@ -26,18 +29,30 @@ def main(args):
 		if k is not None:
 			d = {}
 			c = len(k)
-			print c
 			for item in k:
 				d = {i:item}
-				print d
 				print "[+] Querying: {}, {}".format(url.format(i),d) 
-				result = requests.get(url.format(i), d)
-				# j = json.loads(result.text)
-				print json.dumps(json.loads(result.text), sort_keys=True, indent=4, separators=(',',': '))
-				# print j
+				try:
+					result = requests.get(url.format(i), d)
+					j = json.loads(result.text)
+				except:
+					print "[!] No results available!"
+					sys.exit(1)
+
+				if args.output:
+					try:
+						with open(args.output, 'ab') as o:
+							json.dump(j, o, sort_keys=True, indent=4, separators=(',',': '))
+							o.write("\n\n")
+					except:
+						print "[!] Can't write to a file: {}".format(args.output)
+						sys.exit(1)
+				else:
+					print json.dumps(j, sort_keys=True, indent=4, separators=(',',': '))
+					#print json.dumps(j, indent=4)
 				c -= 1
 				if c > 0:
-					print "[!] Sleep for {} secs...".format(wait)
+					print "[!] Waiting {} secs...".format(wait)
 					time.sleep(wait)
 
 
@@ -46,12 +61,14 @@ if __name__ == '__main__':
 	parser.add_argument('-d', '--domain', nargs='+')
 	parser.add_argument('-i', '--ip', nargs='+')
 	parser.add_argument('-a', '--antivirus', nargs='+')
-	parser.add_argument('-s', '--filehash', nargs='+')
+	parser.add_argument('-s', '--filehash', dest='md5', nargs='+')
 	parser.add_argument('-e', '--email', nargs='+')
-	parser.add_argument('-f', '--file')
 	parser.add_argument('-o', '--output')
+	parser.add_argument('-f', '--file', dest='inFile')
 	args = parser.parse_args()
-	main(args)
-
-
+	if len(sys.argv)==1:
+		parser.print_help()
+		sys.exit(1)
+	else:
+		main(args)
 
